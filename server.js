d073a6ba3289a6ed4658804498938cce682011b8
Marcus Hudnell <hudnell.marcus@gmail.com>
2024-06-05 17:51:51 -0700

    first commit..server and api calls functional

---
+++ server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+const app = express();
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyparser from 'body-parser';
+import OpenAI from 'openai';
+dotenv.config();
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.use(bodyparser.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/api/get-recipe', (req, res) => {
+    res.send('backend')
+})
+
+app.post('/api/get-recipe', async (req, res) => {
+    const { diet } = req.body;
+    console.log(diet)
+    const prompt = `Provide a ${diet} recipe in JSON format with keys of name, servings, ingredients array with quantity,measurement, and instruction (i.e. chopped, diced, etc.) and instructions string array.`;
+
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            max_tokens: 1500,
+            n: 1,
+            stop: null,
+            temperature: 0.6,
+            messages: [{ role: 'user', content: prompt }],
+            response_format: { type: "json_object" }
+        });
+
+        const recipe = response.choices.map(choice => choice.message.content);
+        const parsedRecipe = JSON.parse(recipe);
+        res.json({ recipe: parsedRecipe });
+        console.log(parsedRecipe);
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+        res.status(500).send('Error fetching recipes');
+    }
+});
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is listening to Andre ${port}`)
+}); 
