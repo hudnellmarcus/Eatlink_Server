d8bb8c626789f4f9b9f4bf1df3872e7e354ea682
Marcus Hudnell <hudnell.marcus@gmail.com>
2024-06-12 15:57:14 -0400

    updated cors origin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const openai = new OpenAI({
 });
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // URL of your frontend
+    origin: 'https://eatlink.onrender.com/', // URL of your frontend
     optionsSuccessStatus: 200
 };
 
